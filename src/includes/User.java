e13d096b99982356e8950d81be59d6635d4dc2d6
fanshicomic <fanshicomic@gmail.com>
2014-09-17 15:49:17 +0800

    solve user update Dictionary problems

--- src/includes/User.java
+++ src/includes/User.java
@@ -1,11 +1,20 @@
 package includes;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.Stack;
 
 public class User {
 	ArrayList<Task> CurrentTasks;
 	Stack<ArrayList<Task>> Undoable;
 	Stack<ArrayList<Task>> Redoable;
-	
+	void add(Task task){
+		
+	}
+	void delete(String task_id){
+		
+	}
+	void update(String task_id, Dictionary<String, Object> updateCommand){
+		
+	}
 }
