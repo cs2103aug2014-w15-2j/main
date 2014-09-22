8439df61947077a95cc4139c87942d0b1f714d86
Wang Jinghan <phantom.there@gmail.com>
2014-09-22 13:30:46 +0800

    build the basic backbone in ListOfXiaoMing.java

--- src/ListOfXiaoMing.java
+++ src/ListOfXiaoMing.java
@@ -1,9 +1,91 @@
+import includes.*;
+import data.*;
+
 
 
 public class ListOfXiaoMing {
 
+	//a property to store current user
+	private User thisUser;
+	
+	
+	//user commands set
+	private enum USER_COMMAND {
+		
+	}
+	
 	public static void main(String[] args) {
 		
     }
 	
+	
+	/**
+	 * method to read in an user command and response correspondingly
+	 * @return
+	 */
+	public String readCommand() {
+		
+		return "";
+	}
+	
+	
+	/**
+	 * method for users to log in
+	 * @return
+	 */
+	private String userLogIn() {
+		
+		return "";
+	}
+	
+	
+	/**
+	 * method for users to log out
+	 * @return
+	 */
+	private String userLogOut() {
+		
+		return "";
+	}
+	
+	
+	/**
+	 * method for users to create an account
+	 * @return
+	 */
+	private String createAccount() {
+		
+		return "";
+	}
+	
+	
+	/**
+	 * method for users to get helps
+	 * @return
+	 */
+	private String help() {
+		
+		return"";
+	}
+	
+	/**
+	 * methods to interpret users' input string
+	 * @param userInput
+	 * @return
+	 */
+	private String parse(String userInput) {
+		
+		return "";
+	}
+	
+	
+	/**
+	 * method to execute a command
+	 * @param command
+	 * @return
+	 */
+	private String execute (String command) {
+		
+		return "";
+	}
 }
