e088712916aa83dcce73bdc1e47dd7f5bfca57df
Wang Jinghan <phantom.there@gmail.com>
2014-10-01 19:33:33 +0800

    Toolbox documentation added

--- src/includes/Toolbox.java
+++ src/includes/Toolbox.java
@@ -3,6 +3,11 @@ package includes;
 import java.util.ArrayList;
 
 public class Toolbox {
+	/**
+	 * methods to convert a task list to a string
+	 * @param list
+	 * @return
+	 */
 	public static String taskListToString(ArrayList<Task> list) {
 		String returnValue = "";
 		for (int i = 0; i < list.size(); i++) {
@@ -15,13 +20,31 @@ public class Toolbox {
 		
 		return returnValue;
 	}
+	
+	
+	/**
+	 * to get the first word of a string
+	 * @param userCommand
+	 * @return
+	 */
 	public static String getFirstWord(String userCommand) {
 		String commandTypeString = userCommand.trim().split("\\s+")[0];
 		return commandTypeString;
 	}
+	
+	/**
+	 * to remove the first word of a string
+	 * @param userCommand
+	 * @return
+	 */
 	public static String removeFirstWord(String userCommand) {
 		return userCommand.replace(getFirstWord(userCommand), "").trim();
 	}
+	
+	/**
+	 * print the string
+	 * @param contentsToBeShown
+	 */
 	public static void showToUser(String contentsToBeShown) {
 		System.out.println(contentsToBeShown);
 	}
