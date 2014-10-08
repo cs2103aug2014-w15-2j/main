25b384b3fbe09a7a70e5683ddd7d624768583b61
Wang Jinghan <phantom.there@gmail.com>
2014-10-08 13:45:16 +0800

    add default serialVersionUID for CFException

--- src/dataStructure/CommandFailedException.java
+++ src/dataStructure/CommandFailedException.java
@@ -2,6 +2,11 @@ package dataStructure;
 
 public class CommandFailedException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	//Parameterless Constructor
     public CommandFailedException() {
     	super();
