ac8979f2b0f0a293953fd342083ee9d4cfebd4b5
Wang Jinghan <phantom.there@gmail.com>
2014-10-11 21:26:28 +0800

    make class UtilityMethod abstract

--- src/infrastructure/UtilityMethod.java
+++ src/infrastructure/UtilityMethod.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import dataStructure.Task;
 
-public class UtilityMethod {
+public abstract class UtilityMethod {
 	private static Scanner scanner_ = new Scanner(System.in);
 	public static String readCommand() {
 		return scanner_.nextLine();
