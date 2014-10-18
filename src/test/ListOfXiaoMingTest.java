6b5672bb42933e1cbfb80624c410576906f3af30
Lei Mingyu <uygnim.iel@gmail.com>
2014-10-18 11:50:11 +0800

    test case for undo added

--- src/test/ListOfXiaoMingTest.java
+++ src/test/ListOfXiaoMingTest.java
@@ -15,9 +15,17 @@ public class ListOfXiaoMingTest {
 	
 	@Test
 	public void test() {
-		testOneCommand("Add a task", 
+		testOneCommand("TC01-Add a task", 
 					   "Task added!", 
 					   "add @sampleTask");
+		
+		testOneCommand("TC02a-Undo", 
+					   "Undo successfully!", 
+					   "undo");
+		
+		testOneCommand("TC02b-Undo", 
+					   "--- no task in the list ---      _(:з」∠)_ ", 
+					   "display");
 		
 		System.out.println("all search tests are passed");
 	}
