56f52e2b40a267b03499d4a005aec57c965912b4
Lei Mingyu <uygnim.iel@gmail.com>
2014-10-29 14:30:58 +0800

    test case

--- src/test/ListOfXiaoMingTest.java
+++ src/test/ListOfXiaoMingTest.java
@@ -18,15 +18,23 @@ public class ListOfXiaoMingTest {
 	public void test() {		
 		testOneCommand("TC01-Add a task", 
 					   Constant.PROMPT_MESSAGE_ADD_TASK_SUCCESSFULLY, 
-					   "add @sampleTask");
+					   "add sampleTask");
 		
 		testOneCommand("TC02a-Undo", 
 					   Constant.PROMPT_MESSAGE_UNDO_SUCCESSFULLY, 
 					   "undo");
 		
 		testOneCommand("TC02b-Undo", 
-					   Constant.PROMPT_MESSAGE_NO_TASK_FOUNDED,
+					   Constant.PROMPT_MESSAGE_DISPLAY_EMPTY_TASK,
 					   "display");
+		
+		testOneCommand("TC03a-Add a timed task",
+					   Constant.PROMPT_MESSAGE_ADD_TASK_SUCCESSFULLY, 
+				   	   "add sampleTask today");
+		
+		testOneCommand("TC03b-Search timed task",
+					   "1. sampleTask\n\t priority: medium;\n\t no specific time requirements;",
+					   "search from yesterday to tomorrow");
 		
 		System.out.println("all search tests are passed");
 	}
@@ -34,7 +42,7 @@ public class ListOfXiaoMingTest {
 
 	private void testOneCommand(String description, String expected, String command) {
 		try {
-			assertEquals(description, expected, xiaoming.execute(command)); 
+			assertEquals(description, expected, xiaoming.executeNLP(command)); 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
