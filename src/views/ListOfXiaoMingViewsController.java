b7c1bb6c27f41905eeee1e88ee69434a334c2271
Wang Jinghan <phantom.there@gmail.com>
2014-10-30 23:39:55 +0800

    real time status display for all methods

--- src/views/ListOfXiaoMingViewsController.java
+++ src/views/ListOfXiaoMingViewsController.java
@@ -403,10 +403,6 @@ public class ListOfXiaoMingViewsController extends GridPane implements HotKeyLis
 					setDisplayGrid(this.display());
 					break;
 					
-				case LOG_OUT:
-					setPreview(this.logOut());
-					break;
-					
 				case UNDO:
 					setPreview(this.undo());
 					break;
@@ -446,8 +442,53 @@ public class ListOfXiaoMingViewsController extends GridPane implements HotKeyLis
 
 	public String getPreview(String userInput) {
 		try {
-			Task taskToAdd = parser.nerParser.getTask(userInput);
-			return taskToAdd.toStringForDisplaying();
+			if (userInput.equals("")) {
+				return "The expected result will be shown here";
+			}
+		
+			COMMAND_TYPE thisCommand = this.parser.nerParser.pickCommand(userInput);
+			System.err.println("CMD - getPreview: " + thisCommand);
+			
+			switch(thisCommand) {
+				case ADD:
+					Task taskToAdd = parser.nerParser.getTask(userInput);
+					return "Command: create \n" + taskToAdd.toStringForDisplaying();
+					
+				case DELETE:
+					return "Command: delete";
+					
+				case UPDATE:
+					return "Command: update";
+					
+				case SEARCH:
+					return "Command: search";
+				
+				case DISPLAY:
+					return "Command: display";
+	
+				case UNDO:
+					return "Command: undo";
+					
+				case REDO:
+					return "Command: redo";
+					
+				case CLEAR:
+					return "Command: clear";
+					
+				case EXIT:
+					return "Command: exit";
+					
+				case HELP:
+					return "Command: help";
+					
+				case EMPTY_TRASH:
+					return "Command: empty trash";
+					
+				default:
+					return "Command not recognized";
+	
+			}
+			
 		} catch (CommandFailedException e) {
 			e.printStackTrace();
 			return "Failure in Parsing The Task";
@@ -582,17 +623,6 @@ public class ListOfXiaoMingViewsController extends GridPane implements HotKeyLis
 		} catch (CommandFailedException e) {
 			UtilityMethod.showToUser(e.toString());
 			return Constant.PROMPT_MESSAGE_REDO_FAILED;
-		}
-	}
-
-	private String logOut() {		
-		
-		NERParser.updateModal();
-
-		if (TestingCache.clearCache()) {
-			return Constant.PROMPT_MESSAGE_LOG_OUT_SUCCESSFULLY;
-		} else {
-			return Constant.PROMPT_MESSAGE_CLEAR_CACHE_FAILED;
 		}
 	}
 	
