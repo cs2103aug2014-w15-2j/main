088e33a7406eae230432be7fc0a2fc73637a83ea
Wang Jinghan <phantom.there@gmail.com>
2014-10-30 16:52:37 +0800

    remove useless import

--- src/views/ListOfXiaoMingViewsController.java
+++ src/views/ListOfXiaoMingViewsController.java
@@ -22,7 +22,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class ListOfXiaoMingViewsController extends GridPane implements HotKeyListener{
 	@FXML
