6edf1727cb9c2b5458d691344ca38c7242a60e67
Wang Jinghan <phantom.there@gmail.com>
2014-11-01 19:31:04 +0800

    delete irelavant tests

--- src/dataStore/DataStoreTest.java
+++ src/dataStore/DataStoreTest.java
@@ -19,49 +19,6 @@ public class DataStoreTest {
 	}
 	
 	@Test
-	public void testCreateAccount() {
-		initialize("testFileCA");
-		assertTrue(DataStore.createAccount("testFileCA",
-											"thisIsJustATestFile"));
-		assertFalse(DataStore.createAccount("testFileCA",
-											"thisIsJustATestFile"));
-		File testFile = new File("testFileCA");
-		testFile.delete();
-	}
-	
-	@Test
-	public void testIsAccountExisting() {
-		initialize("testFileAE");
-		DataStore.createAccount("testFileAE", "thisIsJustATestFile");
-		assertTrue(DataStore.isAccountExisting("testFileAE"));
-		assertFalse(DataStore.isAccountExisting("fileNotExisted"));
-		File testFile = new File("testFileAE");
-		testFile.delete();		
-	}
-	
-	@Test
-	public void testAuthenticate() {
-		initialize("testFileA");
-		DataStore.createAccount("testFileA", "thisIsJustATestFile");
-		assertTrue(DataStore.authenticate("testFileA", "thisIsJustATestFile"));
-		assertFalse(DataStore.authenticate("testFileA", "wrongPassword"));
-		assertFalse(DataStore.authenticate("notTestFile",
-											"thisIsJustATestFile"));
-		File testFile = new File("testFileA");
-		testFile.delete();
-	}
-	
-	@Test
-	public void testDestroy() {
-		initialize("testFileD");
-		DataStore.createAccount("testFileD", "thisIsJustATestFile");
-		assertFalse(DataStore.destroy("notTestFile", "thisIsJustATestFile"));
-		assertFalse(DataStore.destroy("testFileD", "wrongPassword"));
-		assertTrue(DataStore.destroy("testFileD", "thisIsJustATestFile"));
-		assertTrue(initialize("testFileD"));
-	}
-	
-	@Test
 	public void testGetCurrentTasks() {
 		initialize("testFileGCT");
 		DataStore.createAccount("testFileGCT", "thisIsJustATestFile");
