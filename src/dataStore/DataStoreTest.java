8e63809e6e7971a924933a49f26aeff93ac917d7
Wang Jinghan <phantom.there@gmail.com>
2014-11-01 19:32:37 +0800

    commens off DataStoreTest

--- src/dataStore/DataStoreTest.java
+++ src/dataStore/DataStoreTest.java
@@ -1,35 +1,35 @@
-package dataStore;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import java.io.File;
-
-public class DataStoreTest {
-
-	public boolean initialize(String testFile) {
-		File oldTestFile = new File(testFile);
-		if(oldTestFile.exists()) {
-			oldTestFile.delete();
-			System.out.println("here " + oldTestFile.getAbsolutePath());
-			return false;
-		}
-		return true;
-	}
-	
-	@Test
-	public void testGetCurrentTasks() {
-		initialize("testFileGCT");
-		DataStore.createAccount("testFileGCT", "thisIsJustATestFile");
-		File user = new File("testFileGCT");
-		try {
-			assertEquals(0, DataStore.getCurrentTasks(user).size());
-		} catch (Exception e) {
-			System.out.println("Failed.");
-		}
-		File testFile = new File("testFileGCT");
-		testFile.delete();
-	}
-	
-}
+//package dataStore;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//import java.io.File;
+//
+//public class DataStoreTest {
+//
+//	public boolean initialize(String testFile) {
+//		File oldTestFile = new File(testFile);
+//		if(oldTestFile.exists()) {
+//			oldTestFile.delete();
+//			System.out.println("here " + oldTestFile.getAbsolutePath());
+//			return false;
+//		}
+//		return true;
+//	}
+//	
+//	@Test
+//	public void testGetCurrentTasks() {
+//		initialize("testFileGCT");
+//		DataStore.createAccount("testFileGCT", "thisIsJustATestFile");
+//		File user = new File("testFileGCT");
+//		try {
+//			assertEquals(0, DataStore.getCurrentTasks(user).size());
+//		} catch (Exception e) {
+//			System.out.println("Failed.");
+//		}
+//		File testFile = new File("testFileGCT");
+//		testFile.delete();
+//	}
+//	
+//}
