aaa026b71b3ad23ccd1eb5a7fd5b6dc43df363e6
Wang Jinghan <phantom.there@gmail.com>
2014-11-01 23:57:45 +0800

    minor improvement in learning

--- src/infrastructure/NERParser.java
+++ src/infrastructure/NERParser.java
@@ -642,15 +642,6 @@ public class NERParser {
 				timeList.add(trivialPair);
 			}
 		}
-
-		Pair<String, String> trivialPair = new Pair<String, String>(
-				".", "O");
-		timeList.add(trivialPair);
-		descriptionList.add(trivialPair);
-		priorityList.add(trivialPair);
-		indexList.add(trivialPair);
-		tagList.add(trivialPair);
-		commandList.add(trivialPair);
 		
 		listMap.put(Constant.XML_TAG_TIME, timeList);
 		listMap.put(Constant.XML_TAG_DESCRIPTION, descriptionList);
