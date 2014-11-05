63fd56971febb577d70b8149d064d3c939544089
Lei Mingyu <uygnim.iel@gmail.com>
2014-11-05 18:22:06 +0800

    rename current tasks to normal tasks in order to avoid confusion

--- src/reference/TaskBox.java
+++ src/reference/TaskBox.java
@@ -6,12 +6,12 @@ import dataStructure.Task;
 
 //@author A0119447Y
 public class TaskBox {
-	public ArrayList<Task> currentTasks;
+	public ArrayList<Task> normalTasks;
 	public ArrayList<Task> finishedTasks;
 	public ArrayList<Task> trashedTasks;
 	
 	public TaskBox() {
-		this.currentTasks = new ArrayList<Task>();
+		this.normalTasks = new ArrayList<Task>();
 		this.finishedTasks = new ArrayList<Task>();
 		this.trashedTasks = new ArrayList<Task>();
 	}
