6116e39b30b3f77d2e272f15fd79978f0904d984
Lei Mingyu <uygnim.iel@gmail.com>
2014-11-05 21:07:19 +0800

    change test case

--- src/test/TaskTest.java
+++ src/test/TaskTest.java
@@ -18,7 +18,7 @@ public class TaskTest {
 		ArrayList<String> tag = new ArrayList<String>();
 		tag.add("cs2102");
 		tag.add("cs2103");
-		Task task1 = new Task("task1", "default", 0, 0, tag, null);
+		Task task1 = new Task("task1", 0, tag, null);
 		
 		testOneCommand("task1 toString", 
 					   "cs2102,cs2103", 
