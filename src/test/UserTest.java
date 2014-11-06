86be87a688d4d56a3a4e986b9345bbdf47ef9cbc
fanshicomic <fanshicomic@gmail.com>
2014-11-06 14:55:25 +0800

    test cases for testing User add method

---
+++ src/test/UserTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import java.util.ArrayList;
+
+import dataStructure.Task;
+import dataStructure.User;
+
+import org.junit.Test;
+
+import reference.TimeInterval;
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+	
+	// @author A0119444E
+	@Test
+	public void testAdd() throws Exception {
+		int testSize = 10;
+		
+		User user = new User();
+		int initialSize = user.getNormalTaskList().size();
+		System.out.println("user normal task size: " + initialSize);
+		
+		ArrayList<String> tag = new ArrayList<String>();
+		TimeInterval interval = new TimeInterval();
+		
+		for (int i = 1; i < testSize + 1; i++){
+			Task task = new Task("task" + i, 2, tag, interval);		
+			user.add(task);
+		}
+		int currentSize = user.getNormalTaskList().size();
+		int p = 1;
+		for (int i = currentSize - testSize; i < currentSize - 1; i++){
+			testAdd("test add for task: " + i, "task" + p++, user, i);
+		}
+		System.out.println("all add tests are passed");
+		for (int i = currentSize - testSize; i < currentSize - 1; i++){
+			user.delete(currentSize - testSize, true);
+		}
+		System.out.println("user normal task size after testing: " + initialSize);
+	}
+	
+	private void testAdd (String description, String expected, User user, int i){
+		try {
+			assertEquals(description, expected, user.retrieveFromNormalList(i).getDescription());
+		} catch (Exception e) {
+			
+		}
+	}
+}
