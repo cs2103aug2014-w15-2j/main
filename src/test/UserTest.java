4229bd1c74c41261bcd8187d46fd006ffe4b8f73
fanshicomic <fanshicomic@gmail.com>
2014-11-06 15:12:45 +0800

    test cases for testing User delete method

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -11,40 +11,81 @@ import reference.TimeInterval;
 import static org.junit.Assert.assertEquals;
 
 public class UserTest {
-	
+
 	// @author A0119444E
+
 	@Test
 	public void testAdd() throws Exception {
 		int testSize = 10;
-		
+
 		User user = new User();
 		int initialSize = user.getNormalTaskList().size();
 		System.out.println("user normal task size: " + initialSize);
-		
+
 		ArrayList<String> tag = new ArrayList<String>();
 		TimeInterval interval = new TimeInterval();
-		
-		for (int i = 1; i < testSize + 1; i++){
-			Task task = new Task("task" + i, 2, tag, interval);		
+
+		for (int i = 1; i < testSize + 1; i++) {
+			Task task = new Task("task" + i, 2, tag, interval);
 			user.add(task);
 		}
 		int currentSize = user.getNormalTaskList().size();
 		int p = 1;
-		for (int i = currentSize - testSize; i < currentSize - 1; i++){
+		for (int i = currentSize - testSize; i < currentSize - 1; i++) {
 			testAdd("test add for task: " + i, "task" + p++, user, i);
 		}
 		System.out.println("all add tests are passed");
-		for (int i = currentSize - testSize; i < currentSize - 1; i++){
+		for (int i = currentSize - testSize; i < currentSize; i++) {
 			user.delete(currentSize - testSize, true);
 		}
-		System.out.println("user normal task size after testing: " + initialSize);
+		
+		System.out.println("user normal task size after testing: "
+				+ user.getNormalTaskList().size());
 	}
 	
-	private void testAdd (String description, String expected, User user, int i){
+	@Test
+	
+	public void testDelete() throws Exception {
+		int testSize = 10;
+
+		User user = new User();
+		int initialSize = user.getNormalTaskList().size();
+		System.out.println("user normal task size: " + initialSize);
+
+		ArrayList<String> tag = new ArrayList<String>();
+		TimeInterval interval = new TimeInterval();
+
+		for (int i = 1; i < testSize + 1; i++) {
+			Task task = new Task("task" + i, 2, tag, interval);
+			user.add(task);
+		}
+		int currentSize = user.getNormalTaskList().size();
+		int p = 1;
+		for (int i = currentSize - testSize; i < currentSize; i++) {
+			testDelete("test delete for task: " + i, "task" + p++, user, currentSize - testSize);
+			user.delete(currentSize - testSize, true);
+		}
+		System.out.println("user normal task size after testing: "
+				+ initialSize);
+		System.out.println("all add tests are passed");		
+	}
+	
+	private void testAdd(String description, String expected, User user, int i) {
 		try {
-			assertEquals(description, expected, user.retrieveFromNormalList(i).getDescription());
+			assertEquals(description, expected, user.retrieveFromNormalList(i)
+					.getDescription());
 		} catch (Exception e) {
-			
+
+		}
+	}
+
+	private void testDelete(String description, String expected, User user,
+			int i) {
+		try {
+			assertEquals(description, expected, user.retrieveFromNormalList(i)
+					.getDescription());
+		} catch (Exception e) {
+
 		}
 	}
 }
