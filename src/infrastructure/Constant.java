8b359990cf677e8fb09c763d2d1bfc176109f4ce
Lei Mingyu <uygnim.iel@gmail.com>
2014-11-07 00:58:09 +0800

    no shadow for text please

--- src/infrastructure/Constant.java
+++ src/infrastructure/Constant.java
@@ -388,8 +388,8 @@ public abstract class Constant {
 	public static final String CSS_STYLE_OVERALL_TIME_BOX = "-fx-padding: 0 0 0 0;"
 			+ "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );";
 	public static final String CSS_STYLE_DESCRIPTION_LABEL = "-fx-font: 17px \"Akagi-SemiBold\";"
-			+ "-fx-padding:0 0 5 0;"
-			+ "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , 5, 0.0 , 0 , 1 );";
+			+ "-fx-padding:0 0 5 0;";
+//			+ "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , z, 0.0 , 0 , 1 );";
 	public static final String CSS_STYLE_TIME_BOX = "-fx-font: 12px \"Akagi-SemiBold\";"
 			+ "-fx-background-color: rgba(0, 0, 0, 0.5);"
 			+ "-fx-padding: 5 0 0 5;"
