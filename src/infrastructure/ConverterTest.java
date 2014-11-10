9d816dd507d7838381aa3be7b7cd9243eb1af67a
gabriellaMT <gabriellamichelle752@gmail.com>
2014-11-10 17:39:45 +0800

    test convertTaskToMap

--- src/infrastructure/ConverterTest.java
+++ src/infrastructure/ConverterTest.java
@@ -22,9 +22,11 @@ public class ConverterTest {
 	@SuppressWarnings("rawtypes")
 	@Test
 	public void testConvertTaskToMapNull() {
-		assertEquals(new LinkedHashMap(), Converter.convertTaskToMap(null));
+		LinkedHashMap taskMap = new LinkedHashMap();
+		assertEquals(taskMap, Converter.convertTaskToMap(null));
 	}
 	
+	@Test
 	@SuppressWarnings("rawtypes")
 	public void testConvertTaskToMapFloating() {
 		ArrayList<String> tags = new ArrayList<String>();
@@ -40,6 +42,7 @@ public class ConverterTest {
 		}
 	}
 	
+	@Test
 	@SuppressWarnings("rawtypes")
 	public void testConvertTaskToMapDeadline() {
 		ArrayList<String> tags = new ArrayList<String>();
@@ -51,12 +54,13 @@ public class ConverterTest {
 			Task task = new Task("task1", Constant.PRIORITY_MEDIUM, tags, time);
 			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
 			assertEquals(Constant.SAVE_FORMAT_NO_DATE, ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_STARTDATE));
-			assertEquals("29-Nov-2014 10:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_ENDDATE));
+			assertEquals("29-November-2014 10:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_ENDDATE));
 		} catch (CommandFailedException | ParseException e) {
 			System.out.println(ERROR_TESTING);
 		}
 	}
 	
+	@Test
 	@SuppressWarnings("rawtypes")
 	public void testConvertTaskToMapPeriod() {
 		ArrayList<String> tags = new ArrayList<String>();
@@ -69,9 +73,102 @@ public class ConverterTest {
 			time = new TimeInterval(startDate, endDate);
 			Task task = new Task("task1", Constant.PRIORITY_MEDIUM, tags, time);
 			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
-			assertEquals("29-Nov-2014 10:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_STARTDATE));
-			assertEquals("29-Nov-2014 12:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_ENDDATE));
+			assertEquals("29-November-2014 10:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_STARTDATE));
+			assertEquals("29-November-2014 12:00", ((LinkedHashMap) taskMap.get(Constant.SAVE_TIME_INTERVAL)).get(Constant.SAVE_ENDDATE));
 		} catch (CommandFailedException | ParseException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapPriorityHigh() {
+		ArrayList<String> tags = new ArrayList<String>();
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_HIGH, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals(Constant.PRIORITY_STRING_HIGH, (taskMap.get(Constant.SAVE_PRIORITY)));
+		} catch (CommandFailedException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapPriorityMedium() {
+		ArrayList<String> tags = new ArrayList<String>();
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_MEDIUM, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals(Constant.PRIORITY_STRING_MEDIUM, (taskMap.get(Constant.SAVE_PRIORITY)));
+		} catch (CommandFailedException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapPriorityLow() {
+		ArrayList<String> tags = new ArrayList<String>();
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_LOW, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals(Constant.PRIORITY_STRING_LOW, (taskMap.get(Constant.SAVE_PRIORITY)));
+		} catch (CommandFailedException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapTagEmpty() {
+		ArrayList<String> tags = new ArrayList<String>();
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_LOW, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals(tags, (taskMap.get(Constant.SAVE_TAGS)));
+		} catch (CommandFailedException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapTags() {
+		ArrayList<String> tags = new ArrayList<String>();
+		tags.add("tag1");
+		tags.add("tag2");
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_LOW, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals(tags, (taskMap.get(Constant.SAVE_TAGS)));
+		} catch (CommandFailedException e) {
+			System.out.println(ERROR_TESTING);
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testConvertTasktoMapDescriptionAndStatus() {
+		ArrayList<String> tags = new ArrayList<String>();
+		TimeInterval time;
+		try {
+			time = new TimeInterval(null, null);
+			Task task = new Task("task1", Constant.PRIORITY_LOW, tags, time);
+			LinkedHashMap taskMap = Converter.convertTaskToMap(task);
+			assertEquals("task1", taskMap.get(Constant.SAVE_DESCRIPTION));
+			assertEquals(Constant.TASK_STATUS_ONGOING, taskMap.get(Constant.SAVE_STATUS));
+		} catch (CommandFailedException e) {
 			System.out.println(ERROR_TESTING);
 		}
 	}
