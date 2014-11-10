249d9a2c223b0ef2cfeb009f2e8f1e9c3fca58a3
gabriellaMT <gabriellamichelle752@gmail.com>
2014-11-10 16:17:30 +0800

    add logging, fix formating

--- src/dataStore/DataStore.java
+++ src/dataStore/DataStore.java
@@ -3,46 +3,54 @@
 package dataStore;
 
 import infrastructure.Converter;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.io.IOException;
-
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 import model.Task;
 import model.TaskBox;
+
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 
+import com.sun.media.jfxmedia.logging.Logger;
+
 public abstract class DataStore {
 
-	private static final String DATA_FILEPATH = 
-								"List-of-Xiao-Ming/task-list.xiaoming";
-	
+	private static final String DATA_FILEPATH = "List-of-Xiao-Ming/task-list.xiaoming";
+
 	/**
-	 * get the tasks list from the file data
-	 * make a new file data if does not exist
+	 * get the tasks list from the file data make a new file data if does not
+	 * exist
+	 * 
 	 * @return TaskBox for the tasks list
+	 * @throws Exception 
+	 * 
 	 */
-	public static TaskBox loadFileData() throws Exception {
-		if(!isFileExisting()) {
+	public static TaskBox loadFileData() throws Exception{
+		if (!isFileExisting()) {
 			createTaskFile();
 		}
 		return getCurrentTasks();
 	}
-	
+
 	/**
-	 * save the changes, write all tasks into the account data 
-	 * @return true if succeed, false otherwise
+	 * save the changes, write all tasks into the account data
+	 * 
+	 * @param tasks - list of tasks need to be saved into the file
+	 * @return true if it is successfully saved, false otherwise  
 	 */
 	@SuppressWarnings("rawtypes")
 	public static boolean save(TaskBox tasks) {
-		assert(isFileExisting());
+		assert (isFileExisting());
 		if (!isFileExisting()) {
 			return false;
 		}
@@ -54,14 +62,16 @@ public abstract class DataStore {
 			fw.write(writer.toString());
 			writer.close();
 			fw.close();
+			Logger.logMsg(Logger.INFO, "Content saved to file");
 			return true;
 		} catch (IOException e) {
 			return false;
-		}				
+		}
 	}
 
 	/**
 	 * check whether the file-data exists
+	 * 
 	 * @return true if exists, no otherwise
 	 */
 	private static boolean isFileExisting() {
@@ -71,105 +81,116 @@ public abstract class DataStore {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * create a new data file
+	 * 
 	 * @return true if succeed, false otherwise
 	 */
 	private static boolean createTaskFile() {
-		assert(!isFileExisting());
+		assert (!isFileExisting());
 		if (isFileExisting()) {
+			Logger.logMsg(Logger.INFO, "File already exists");
 			return false;
 		}
 		try {
 			File fileData = new File(DATA_FILEPATH);
 			fileData.createNewFile();
 			save(null);
+			Logger.logMsg(Logger.INFO, "New file created");
 			return true;
 		} catch (IOException e) {
+			Logger.logMsg(Logger.INFO, "Failed in creating a new file");
 			return false;
 		}
 	}
-	
+
 	/**
 	 * read file and get user current tasks
+	 * 
 	 * @return user current tasks
+	 * @throws Exception - when it fails reading from the file,
+	 * 					   when the file does not exist
+	 * 					   or when the parsing fails  
 	 */
 	@SuppressWarnings("rawtypes")
-	private static TaskBox getCurrentTasks() throws Exception {
-		assert(isFileExisting());
+	private static TaskBox getCurrentTasks() throws Exception{
+		assert (isFileExisting());
 		TaskBox tasksList = new TaskBox();
-		
+
 		FileReader user = new FileReader(DATA_FILEPATH);
 		JSONParser parser = new JSONParser();
 		ContainerFactory orderedKeyFactory = setOrderedKeyFactory();
 		ArrayList allTasks = (ArrayList) parser.parse(user, orderedKeyFactory);
 		
 		LinkedHashMap task;
-		if(allTasks != null) {
-			for(int i = 0; i < allTasks.size(); i++) {
+		if (allTasks != null) {
+			for (int i = 0; i < allTasks.size(); i++) {
 				task = (LinkedHashMap) allTasks.get(i);
 				Task newTask = Converter.convertMapToTask(task);
-				if(newTask.isDone()) {
+				if (newTask.isDone()) {
 					tasksList.getFinishedTasks().add(newTask);
-				} else if(newTask.isTrashed()) {
+				} else if (newTask.isTrashed()) {
 					tasksList.getTrashedTasks().add(newTask);
 				} else {
-					//default: ongoing normal tasks
+					// default: ongoing normal tasks
 					tasksList.getNormalTasks().add(newTask);
 				}
 			}
 		}
 		
 		user.close();
+		Logger.logMsg(Logger.INFO, "All tasks loaded");
 		return tasksList;
 	}
-	
+
 	/**
+	 * convert a list of tasks into the available format for the file 
+	 * 
 	 * @return the content of the file to be written
 	 */
 	@SuppressWarnings({ "rawtypes" })
 	private static ArrayList getContent(TaskBox tasks) {
-		//list of all tasks
+		// list of all tasks
 		ArrayList<LinkedHashMap> tasksList = new ArrayList<LinkedHashMap>();
-		
-		if( tasks != null) {
-			for(int i = 0; i < tasks.getNormalTasks().size(); i++) {
-				LinkedHashMap task = Converter.convertTaskToMap
-									(tasks.getNormalTasks().get(i));
+
+		if (tasks != null) {
+			for (int i = 0; i < tasks.getNormalTasks().size(); i++) {
+				LinkedHashMap task = Converter.convertTaskToMap(tasks
+						.getNormalTasks().get(i));
 				tasksList.add(task);
 			}
-			for(int i = 0; i < tasks.getFinishedTasks().size(); i++) {
-				LinkedHashMap task = Converter.convertTaskToMap
-									(tasks.getFinishedTasks().get(i));
+			for (int i = 0; i < tasks.getFinishedTasks().size(); i++) {
+				LinkedHashMap task = Converter.convertTaskToMap(tasks
+						.getFinishedTasks().get(i));
 				tasksList.add(task);
 			}
-			for(int i = 0; i < tasks.getTrashedTasks().size(); i++) {
-				LinkedHashMap task = Converter.convertTaskToMap
-									(tasks.getTrashedTasks().get(i));
+			for (int i = 0; i < tasks.getTrashedTasks().size(); i++) {
+				LinkedHashMap task = Converter.convertTaskToMap(tasks
+						.getTrashedTasks().get(i));
 				tasksList.add(task);
 			}
 		}
 		
+		Logger.logMsg(Logger.INFO, "All tasks converted to content");
 		return tasksList;
 	}
-	
+
 	/**
 	 * @return an ordered key factory (maintain the ordering from the file)
 	 */
 	@SuppressWarnings("rawtypes")
 	private static ContainerFactory setOrderedKeyFactory() {
-		ContainerFactory orderedKeyFactory = new ContainerFactory()
-		{
+		ContainerFactory orderedKeyFactory = new ContainerFactory() {
 			@Override
-		    public List creatArrayContainer() {
-		      return new ArrayList();
-		    }
+			public List creatArrayContainer() {
+				return new ArrayList();
+			}
 
 			@Override
 			public Map createObjectContainer() {
-		      return new LinkedHashMap();
-		    }
+				return new LinkedHashMap();
+			}
 		};
 		return orderedKeyFactory;
 	}
--- src/infrastructure/Converter.java
+++ src/infrastructure/Converter.java
@@ -14,133 +14,133 @@ import model.Task;
 import model.TimeInterval;
 
 public class Converter {
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static LinkedHashMap convertTaskToMap(Task task) {
 		LinkedHashMap taskMap = new LinkedHashMap();
-		
+
 		taskMap.put(Constant.SAVE_DESCRIPTION, task.getDescription());
 		taskMap.put(Constant.SAVE_STATUS, task.getStatus());
-		
+
 		ArrayList<String> tags = new ArrayList<String>();
-		for(int i = 0; i < task.getTag().size(); i++) {
+		for (int i = 0; i < task.getTag().size(); i++) {
 			tags.add(task.getTag().get(i));
 		}
 		taskMap.put(Constant.SAVE_TAGS, tags);
-	
-		taskMap.put(Constant.SAVE_PRIORITY, 
-					convertPriorityIntToString(task.getPriority()));
+
+		taskMap.put(Constant.SAVE_PRIORITY,
+				convertPriorityIntToString(task.getPriority()));
 		taskMap.put(Constant.SAVE_TIME_INTERVAL,
-					convertTimeIntervalToString(task.getInterval()));
-		
+				convertTimeIntervalToString(task.getInterval()));
+
 		return taskMap;
 	}
-	
+
 	/**
 	 * convert map to task
+	 * 
 	 * @param task
 	 * @return
 	 * @throws Exception
 	 */
-	@SuppressWarnings("rawtypes") 
+	@SuppressWarnings("rawtypes")
 	public static Task convertMapToTask(LinkedHashMap task) throws Exception {
 		String description = (String) task.get(Constant.SAVE_DESCRIPTION);
 		String status = (String) task.get(Constant.SAVE_STATUS);
-		int priority = convertPriorityStringToInt
-						((String) task.get(Constant.SAVE_PRIORITY));
-		
+		int priority = convertPriorityStringToInt((String) task
+				.get(Constant.SAVE_PRIORITY));
+
 		ArrayList tags = (ArrayList) task.get(Constant.SAVE_TAGS);
 		ArrayList<String> tag = new ArrayList<String>();
-		for(int i = 0; i < tags.size(); i++) { 
-			tag.add((String)tags.get(i));
+		for (int i = 0; i < tags.size(); i++) {
+			tag.add((String) tags.get(i));
 		}
-		
-		LinkedHashMap intervalObj = (LinkedHashMap) 
-									task.get(Constant.SAVE_TIME_INTERVAL);
+
+		LinkedHashMap intervalObj = (LinkedHashMap) task
+				.get(Constant.SAVE_TIME_INTERVAL);
 		TimeInterval interval = convertStringToTimeInterval(intervalObj);
-		
+
 		Task newTask = new Task(description, priority, tag, interval);
 		newTask.setStatus(status);
-		
+
 		return newTask;
 	}
 
 	private static int convertPriorityStringToInt(String priorityString) {
 		priorityString = priorityString.toLowerCase().trim();
 		int priority = Constant.PRIORITY_DEFAULT;
-		if(priorityString.equals(Constant.PRIORITY_STRING_HIGH)) {
+		if (priorityString.equals(Constant.PRIORITY_STRING_HIGH)) {
 			priority = Constant.PRIORITY_HIGH;
-		} else if(priorityString.equals(Constant.PRIORITY_STRING_MEDIUM)) {
+		} else if (priorityString.equals(Constant.PRIORITY_STRING_MEDIUM)) {
 			priority = Constant.PRIORITY_MEDIUM;
-		} else if(priorityString.equals(Constant.PRIORITY_STRING_LOW)) {
+		} else if (priorityString.equals(Constant.PRIORITY_STRING_LOW)) {
 			priority = Constant.PRIORITY_LOW;
 		}
 		return priority;
 	}
-	
+
 	private static String convertPriorityIntToString(int priorityInt) {
 		String priority;
-		switch(priorityInt) {
-			case Constant.PRIORITY_HIGH:
-				priority = Constant.PRIORITY_STRING_HIGH;
-				break;
-			case Constant.PRIORITY_MEDIUM:
-				priority = Constant.PRIORITY_STRING_MEDIUM;
-				break;
-			case Constant.PRIORITY_LOW:
-				priority = Constant.PRIORITY_STRING_LOW;
-				break;
-			default:
-				priority = Constant.PRIORITY_STRING_MEDIUM;
-				break;
+		switch (priorityInt) {
+		case Constant.PRIORITY_HIGH:
+			priority = Constant.PRIORITY_STRING_HIGH;
+			break;
+		case Constant.PRIORITY_MEDIUM:
+			priority = Constant.PRIORITY_STRING_MEDIUM;
+			break;
+		case Constant.PRIORITY_LOW:
+			priority = Constant.PRIORITY_STRING_LOW;
+			break;
+		default:
+			priority = Constant.PRIORITY_STRING_MEDIUM;
+			break;
 		}
 		return priority;
 	}
 
 	@SuppressWarnings("rawtypes")
-	private static TimeInterval convertStringToTimeInterval
-								(LinkedHashMap intervalObj) 
-								throws ParseException, CommandFailedException {
-		Date startDate = convertDateStringToDate
-						( (String) intervalObj.get(Constant.SAVE_STARTDATE)); 
-		Date endDate = convertDateStringToDate
-						( (String) intervalObj.get(Constant.SAVE_ENDDATE));
+	private static TimeInterval convertStringToTimeInterval(
+			LinkedHashMap intervalObj) throws ParseException,
+			CommandFailedException {
+		Date startDate = convertDateStringToDate((String) intervalObj
+				.get(Constant.SAVE_STARTDATE));
+		Date endDate = convertDateStringToDate((String) intervalObj
+				.get(Constant.SAVE_ENDDATE));
 		return new TimeInterval(startDate, endDate);
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static LinkedHashMap convertTimeIntervalToString
-											(TimeInterval time) {
+	private static LinkedHashMap convertTimeIntervalToString(TimeInterval time) {
 		LinkedHashMap timeInterval = new LinkedHashMap();
-		
+
 		String startDate = convertDateToString(time.getStartDate());
 		timeInterval.put(Constant.SAVE_STARTDATE, startDate);
 		String endDate = convertDateToString(time.getEndDate());
 		timeInterval.put(Constant.SAVE_ENDDATE, endDate);
-		
+
 		return timeInterval;
 	}
-	
+
 	private static String convertDateToString(Date date) {
 		String dateString;
-		if( date.equals(Constant.FLOATING_START_DATE) ||
-			date.equals(Constant.FLOATING_END_DATE) ||
-			date.equals(Constant.DEADLINE_START_DATE) ) {
-			dateString = Constant.SAVE_FORMAT_NO_DATE; 
+		if (date.equals(Constant.FLOATING_START_DATE)
+				|| date.equals(Constant.FLOATING_END_DATE)
+				|| date.equals(Constant.DEADLINE_START_DATE)) {
+			dateString = Constant.SAVE_FORMAT_NO_DATE;
 		} else {
 			dateString = new SimpleDateFormat(Constant.FORMAT_DATE,
-					Locale.ENGLISH).format(date);	
+					Locale.ENGLISH).format(date);
 		}
 		return dateString;
 	}
-	
+
 	private static Date convertDateStringToDate(String dateString)
-												throws ParseException {
-		if(dateString.trim().equals(Constant.SAVE_FORMAT_NO_DATE)) {
+			throws ParseException {
+		if (dateString.trim().equals(Constant.SAVE_FORMAT_NO_DATE)) {
 			return null;
 		}
-		return new SimpleDateFormat(Constant.FORMAT_DATE,
-				Locale.ENGLISH).parse(dateString);
+		return new SimpleDateFormat(Constant.FORMAT_DATE, Locale.ENGLISH)
+				.parse(dateString);
 	}
 	
 	/*
